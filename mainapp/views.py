0a1d2a8e2ad24e1adf551f6949cafb2d52881f32
sl <ricar@mail.ru>
2022-11-24 13:28:03 +0300

    add json news

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,3 +1,4 @@
+import json
 from datetime import datetime
 
 from django.views.generic import TemplateView
@@ -16,6 +17,21 @@ class NewsPageView(TemplateView):
         # Create your own data
         context["news_title"] = "Громкий новостной заголовок"
         context["news_preview"] = "Предварительное описание, которое заинтересует каждого"
+        context["range"] = range(5)
+        context["datetime_obj"] = datetime.now()
+        return context
+
+
+class News1PageView(TemplateView):
+    template_name = "mainapp/news1.html"
+
+    def get_context_data(self, **kwargs):
+        # Get all previous data
+        context = super().get_context_data(**kwargs)
+        # Create your own data
+        context["news_title"] = "Громкий новостной заголовок"
+        with open("static/news.json", "r") as file_1:
+            context["news_preview"] = json.load(file_1)
         context["range"] = range(5)
         context["datetime_obj"] = datetime.now()
         return context
