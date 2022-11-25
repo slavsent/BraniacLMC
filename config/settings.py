86b67f71dd39b9c63e609797be4f5dbcb6878e9e
sl <ricar@mail.ru>
2022-11-25 15:39:49 +0300

    lesson_4 Display model's data

--- config/settings.py
+++ config/settings.py
@@ -36,8 +36,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "mainapp",
+    "markdownify.apps.MarkdownifyConfig",
     "multiforloop",
+    "mainapp",
     
 ]
 
