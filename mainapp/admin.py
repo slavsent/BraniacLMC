e082c2cd5ea10d6281bd62b785c5126856c0621d
sl <ricar@mail.ru>
2022-12-03 20:31:39 +0300

    lesson_6 Customizing admin panel

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -1,1 +1,36 @@
-# Register your models here.
+from django.contrib import admin
+from django.utils.html import format_html
+from django.utils.translation import gettext_lazy as _
+
+from mainapp import models as mainapp_models
+
+
+@admin.register(mainapp_models.News)
+class NewsAdmin(admin.ModelAdmin):
+    search_fields = ["title", "preambule", "body"]
+
+
+@admin.register(mainapp_models.Lesson)
+class LessonAdmin(admin.ModelAdmin):
+    list_display = ["id", "get_course_name", "num", "title", "deleted", "slug"]
+    ordering = ["-course__name", "-num"]
+    list_per_page = 5
+    list_filter = ["course", "created", "deleted"]
+    actions = ["mark_deleted"]
+
+    def get_course_name(self, obj):
+        return obj.course.name
+
+    get_course_name.short_description = _("Course")
+
+    def slug(self, obj):
+        return format_html(
+            '<a href="{}" target="_blank">{}</a>',
+            obj.title.lower().replace(' ', '-'),
+            obj.title
+        )
+
+    def mark_deleted(self, request, queryset):
+        queryset.update(deleted=True)
+
+    mark_deleted.short_description = _("Mark deleted")
