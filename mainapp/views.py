1a5dfc0d9a95b3b04c38f27eb06dc49176920bfb
sl <ricar@mail.ru>
2022-12-09 15:32:19 +0300

    lesson_8 add new courses plagination

--- mainapp/views.py
+++ mainapp/views.py
@@ -57,14 +57,19 @@ class NewsDeleteView(PermissionRequiredMixin, DeleteView):
     permission_required = ("mainapp.delete_news",)
 
 
-class CoursesListView(TemplateView):
+class CoursesListView(ListView):
     template_name = "mainapp/courses_list.html"
-
+    model = mainapp_models.Courses
+    paginate_by = 6
+    
     def get_context_data(self, **kwargs):
         context = super(CoursesListView, self).get_context_data(**kwargs)
-        context["objects"] = mainapp_models.Courses.objects.all()[:7]
+        context["objects"] = mainapp_models.Courses.objects.all
+        
+        #context["objects"] = mainapp_models.Courses.objects.all()[:6]
         return context
 
+    
 
 class CoursesDetailView(TemplateView):
     template_name = "mainapp/courses_detail.html"
